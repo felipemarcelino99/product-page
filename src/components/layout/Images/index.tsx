7c0f27fd8b82b35ba7e2b12090c48dc50416e78d
Felipe Marcelino <felipe99marcelino@hotmail.com>
2025-05-21 15:54:56 -0300

    feat: add react-inner-image-zoom

--- src/components/layout/Images/index.tsx
+++ src/components/layout/Images/index.tsx
@@ -1,8 +1,10 @@
 import Image from "../../common/Image";
 import { useProduct } from "../../../contexts/ProductContext";
+import InnerImageZoom from "react-inner-image-zoom";
+import "react-inner-image-zoom/lib/styles.min.css";
 
 export function Images() {
-  const { images, setPrincipalImage, principalImage, variant } = useProduct();
+  const { images, setPrincipalImage, principalImage } = useProduct();
 
   return (
     <div className="flex items-start gap-1.5 max-w-[47%] flex-1">
@@ -17,10 +19,12 @@ export function Images() {
           );
         })}
       </div>
-      <div className="w-full">
-        <img
-          src={principalImage}
-          alt={`Calça na cor ${variant?.value}`}
+      <div className="w-full relative overflow-hidden group max-w-[1024px]">
+        <InnerImageZoom
+          src={principalImage ?? ""}
+          zoomType="hover"
+          zoomPreload={true}
+          zoomScale={1.5}
           width={1024}
           height={1024}
         />
